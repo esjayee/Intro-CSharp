e1228c4a582f43345c1acb897e5cf25679cb9d52
susan.emmons <susan.emmons@AG007075>
2018-08-01 13:17:22 +0100

    Latest changes before lunch

--- Mod3_Lab1/Mod3_Lab1/Program.cs
+++ Mod3_Lab1/Mod3_Lab1/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mod3_Lab1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
 
+        }
+
+        // Sum() method that takes two integer arguments and sums them
+        // The method returns no value which is why we use void
+        // We also need to use static in the method signature because Main is static
+        // and you cannot call a non-static method from a static method
+
+        static void Sum(int first, int second)
+        {
+            int sum = first + second;
+            Console.WriteLine($"The sum of {first} and {second} is: {sum}");
         }
     }
 }
